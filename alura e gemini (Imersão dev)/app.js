7662383f944bf5675c2f5b543f1dcb38793d4556
Levi-developer <renalevi41@gmail.com>
2024-09-07 13:35:33 -0300

    adicionando codigo final

--- alura e gemini (Imersão dev)/app.js
+++ alura e gemini (Imersão dev)/app.js
@@ -1,18 +1,38 @@
-
 function pesquisar() {
-let section = document.getElementById('resultados-pesquisa'); 
-let resultados = ''
+    let campoPesquisa = document.getElementById('campo-pesquisa').value
+    let section = document.getElementById('resultados-pesquisa'); 
+    let resultados = '';
+    let titulo = '';
+    let descricao = '';
 
-for(let dado of dados) {
-    resultados += `
-   <div class="item-resultado">
-        <h2>${dado.titulo}</h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href="${dado.link}" target="_blank">Mais informações</a>
-    </div>
-`
-}
+    if (campoPesquisa == "") {
+        section.innerHTML = "<p><strong>[ERRO]</strong> Campo vazio ou atleta não encontrado!</p>"
+        return
+    }
 
-section.innerHTML = resultados
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    for(let dado of dados) {
+
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+            resultados += `
+            <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+            `
+        }
+    }
+
+    if (!resultados) {
+        section.innerHTML = "<p><strong>[ERRO]</strong> Campo vazio ou atleta não encontrado!</p>"
+        return
+    }
+
+    section.innerHTML = resultados
 
 }
