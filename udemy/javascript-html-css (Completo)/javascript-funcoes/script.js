1a246a759f34fffd3268b83340dbf1a464d07ce6
Levi-developer <renalevi41@gmail.com>
2024-09-13 17:26:25 -0300

    concerto + pasta udemy

---
+++ udemy/javascript-html-css (Completo)/javascript-funcoes/script.js
@@ -0,0 +1,15 @@
+    /* Funções */ 
+
+function minhafuncao() {
+    return console.log ('Hello World')
+}
+
+minhafuncao()
+
+/* Parametros da função*/ 
+
+function nome(idade) {
+    return idade * idade
+}
+
+console.log (nome(10))
---
+++ udemy/javascript-html-css (Completo)/javascript/script.js
@@ -0,0 +1,48 @@
+var nome = "Levi Silva Vieira"
+var numero = 100
+
+var verdadeiro = true
+var falso = false
+
+console.log(nome, numero)
+console.log(verdadeiro, falso)
+
+/* operadores*/
+    /* operadores de atribuição*/
+
+var primeironumero = 5
+var segundonumero = 6
+
+console.log(primeironumero += segundonumero)
+
+    /* operadores aritimeticos */
+
+var a = 2 + 3
+var b = 2 - 3
+var c = 2 * 3
+var d = 2 / 3
+
+var e = 2 % 3
+var f = 2 ** 3 /* raiz quadrada*/
+
+    /* operadores relacionais */
+
+ /* > maior que */
+ /* < menor que */
+ /* >= maior ou igual*/
+ /* <= menor ou igual */
+
+ /* == igual */
+ /* === Retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo.*/
+
+var maior = 10 > 5
+var menor = 5 < 10
+var maiorouigual = 10 >= 10
+var menorouigual = 5 <= 10
+
+var igual = 10 == '10'
+var igualdade = 10 === '10'
+var igualnumero = 10 === 10
+
+console.log (maior, menor, maiorouigual, menorouigual) 
+console.log (igual, igualdade, igualnumero)
